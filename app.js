9fc3b2756f9116b27f296ad26b83e3de2de218af
alejandrob318 <alejandroblanco318@gmail.com>
2025-01-25 05:10:18 -0300

    agregando 4 fucionalidades

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function validarEntrada(){
+    let amigo = document.getElementById("amigo").value;
+    if(amigo.trim() == ""){
+        alert("por favor ingrese un numero valido");
+        
+    }else{
+        agregarAmigo(amigo);
+        listarAmigos(amigo);
+    }
+}
+function agregarAmigo(amigo){
+    amigos.push(amigo);
+}
+
+function listarAmigos(amigo){
+    let lista = document.querySelector("#listaAmigos");
+    let nombre = document.createElement("li");
+    nombre.textContent = amigo; 
+    lista.appendChild(nombre);
+
+}
+
+function sortearAmigo(){
+    let amigoElegido = parseInt(Math.floor(Math.random()*amigos.length));
+    return amigos[amigoElegido];
+    
+}
+function mostrarAmigo(){
+    let amigo = document.querySelector("#resultado");
+    amigo.innerHTML = `el amigo sorteado es: ${sortearAmigo()}`
+
+}
+
